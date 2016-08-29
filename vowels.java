6a4d0df2ff80209ae45226ad72e657d8297cffcf
kalai47 <kalaivanicse96@gmail.com>
2016-08-29 10:28:12 +0530

    Create vowels.java

---
+++ vowels.java
@@ -0,0 +1,18 @@
+public class vowels
+{
+public static void main(String args[])
+{
+char ch;
+Scanner s=new Scanner(System.in);
+System.out.println("enter the alaphat");
+ch=scan.next.charAt(0);
+if(ch=='a'|| ch=='A' || ch=='e' || ch='E'|| ch=='i' || ch=='I' || ch=='o' ||ch=='O' ch=='u'|| ch=='U')
+{
+System.out.println("This is a vowel");
+}
+else
+{
+System.out.println("This is not a vowel");
+}
+}
+}
